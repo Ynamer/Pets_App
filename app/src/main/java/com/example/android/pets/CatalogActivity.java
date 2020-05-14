1d5a6cb0608bd752beab7ffedb31be4512701bf0
Yasir <yasir99as@gmail.com>
2020-05-14 22:46:26 +0300

    after add single pet to database by activity

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -40,8 +40,14 @@ public class CatalogActivity extends AppCompatActivity {
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
         mDbHelper = new PetDbHelper(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         displayDatabaseInfo();
     }
+
 
     /**
      * Temporary helper method to display information in the onscreen TextView about the state of
